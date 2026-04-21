a225b9192d4dbc24e6fe57332f3c212a39ceeec5
Anil Reddy <nagireddyanilreddy750@gmail.com>
2026-04-21 14:08:24 +0530

    Added reverse.js file

---
+++ reverse.js
@@ -0,0 +1,4 @@
+var rev=["anil","Karthik"];
+for (var i = rev.length - 1; i >= 0; i--){
+        console.log(rev[i]);
+}
---
+++ reversest.js
@@ -0,0 +1,6 @@
+var revst="anilkarthik";
+var str="";
+for (var i=revst.length-1;i>=0;i--){
+    console.log(str);
+    str=revst[i];
+}
