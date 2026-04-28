6e35d4225f4e00b84f80932ba643b4ffea3320b5
Anil Reddy <nagireddyanilreddy750@gmail.com>
2026-04-28 18:06:44 +0530

    scope.js

---
+++ codingpr/scope.js
@@ -0,0 +1,29 @@
+function add(){
+    var x=10;
+    if(true){
+        let x=15;
+    }
+    console.log("x-----",x);
+}
+if(true){
+    var num=15;
+    console.log("block---",num);
+}
+add();
+var num=2;
+function parent(){
+    var property =100;
+    function child(){
+        console.log("child---",property);
+        var adddproperty=50;
+        function subchild(){
+            console.log("subchild---",property);
+            var doubleproperty=30;
+
+        }
+        subchild()
+    }
+    child()
+
+}
+parent()
