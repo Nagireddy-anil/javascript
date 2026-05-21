40653109e5aa02aff2dafd47bc9db7efd0f799b4
Anil Reddy <nagireddyanilreddy750@gmail.com>
2026-05-21 19:52:26 +0530

    recursive.js

---
+++ codingpr/recursive.js
@@ -0,0 +1,19 @@
+const userkey={
+    user:{
+        profile:{
+            name:'anil',
+            age:21,
+            height:5.7
+        }
+    }
+   
+}
+//  console.log(Object.keys(userkey));
+ console.log(Object.keys(userkey.user.profile));
+ /*if(userkey.user){
+    console.log("1st block");
+ }*/
+ /* a fun itself is called recursive fun*/
+ // fiond key based on value
+ // foind value based on key
+ // fond duplicate keys
