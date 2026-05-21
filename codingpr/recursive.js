335a73ceb5183fcf6d6c7eee8bd9d8582a49abe7
Anil Reddy <nagireddyanilreddy750@gmail.com>
2026-05-21 20:17:21 +0530

    update code

--- codingpr/recursive.js
+++ codingpr/recursive.js
@@ -5,11 +5,25 @@ const userkey={
             age:21,
             height:5.7
         }
+    },
+    user1: {
+        city: "Hyderabad"
+    },
+    user2: {
+        city: "Delhi"
+    },
+    user3: {
+        city: "Chennai"
     }
    
 }
 //  console.log(Object.keys(userkey));
- console.log(Object.keys(userkey.user.profile));
+//  console.log(Object.keys(userkey.user.profile));
+ for (let key in userkey){
+    if(userkey[key].city){
+    console.log(userkey[key].city);
+ }
+}
  /*if(userkey.user){
     console.log("1st block");
  }*/
